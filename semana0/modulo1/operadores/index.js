22da4148dc73f77e8cb933b1bfaa5e7bd6933604
alisson abilio <alissonabilio@gmail.com>
2021-10-05 16:36:13 -0300

    AULA DE OPERADORES LOGICOS

---
+++ semana0/modulo1/operadores/index.js
@@ -0,0 +1,95 @@
+// const bool1 = true
+// const bool2 = false
+// const bool3 = !bool2
+
+// let resultado = bool1 && bool2
+// console.log("a. ", resultado)
+// // RESPOSTA: FALSE
+// resultado = bool1 && bool2 && bool3 
+// console.log("b. ", resultado) 
+// // RESPOSTA: FALSE
+// resultado = !resultado && (bool1 || bool2) 
+// console.log("c. ", resultado)
+// // RESPOSTA: NÃO SEI 
+// console.log("d. ", typeof resultado)
+// // RESPOSTA: BOOLEAN
+
+//-----------------------
+ 
+
+//SERA IMPRESSO NO CONSOLE A CONCATENASAO DAS DUAS STRING
+
+
+//----------------------
+
+// let primeiroNumero = prompt("Digite um numero!")
+// let segundoNumero = prompt("Digite outro numero!")
+
+// const soma = Number(primeiroNumero) + Number(segundoNumero)
+
+// console.log(soma)
+
+
+//------------------------
+
+
+// let suaIdade = prompt(  "QUAL A SUA IDADE?")
+// let idadeDaMelhorAmiga = prompt(  "QUAL A SUA IDADE DA SUA MELHOR AMIGA?")
+
+// let contaracao = Number(suaIdade) >= Number(idadeDaMelhorAmiga)
+
+// console.log( "Sua idade é maior do que a do seu melhor amigo?", contaracao)
+
+// let diferenca = suaIdade - idadeDaMelhorAmiga
+
+
+//-----------------------------
+
+// let InserirNumeroPar = prompt ("Por favor inserir um número par.")
+
+// let resto = Number(InserirNumeroPar) % 2
+
+// console.log(resto)
+
+// // quando resta 1 o número é impar.
+// // quando resta 0 o número é par.
+
+
+//----------------------------------
+
+// let suaIdade = prompt("QUAL A SUA IDADE EM ANOS?")
+
+// let idadeEmMeses = Number(suaIdade) * 12
+// let idadeEmDias = Number(idadeEmMeses) * 30
+// let idadeEmHORAS = Number(idadeEmDias) * 24
+
+// console.log("sua idade em anos é",suaIdade,".")
+// console.log("sua idade em meses é",idadeEmMeses,".")
+// console.log("sua idade em dias é",idadeEmDias,".")
+// console.log("sua idade em horas é",idadeEmHORAS,".")
+
+
+//------------------------------------------
+
+
+// let primeiroNumero = prompt("inserir um número.")
+// let segundoNumero = prompt("inserir um número.")
+
+// let maior = primeiroNumero > segundoNumero
+// let igual = primeiroNumero === segundoNumero
+// let divisivel = primeiroNumero % segundoNumero
+// let resto = divisivel === 0 
+// let divisivel2 = segundoNumero % primeiroNumero
+// let resto2 = divisivel2 ===0 
+// // O primeiro numero é maior que segundo? true
+// // O primeiro numero é igual ao segundo? false
+// // O primeiro numero é divisível pelo segundo? true
+// // O segundo numero é divisível pelo primeiro?
+
+// console.log(maior)
+// console.log(igual)
+// console.log(resto)
+// console.log(resto2)
+
+//-----------------------------------------------
+
