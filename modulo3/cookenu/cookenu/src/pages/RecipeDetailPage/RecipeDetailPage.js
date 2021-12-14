2b06a4d0b2953b93d7573baf08da9e34c4d53aeb
alisson <alissonabilio@gmail.com>
2021-12-13 21:34:39 -0300

    estrutura do projeto

---
+++ modulo3/cookenu/cookenu/src/pages/RecipeDetailPage/RecipeDetailPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const RecipeDetailPage =() =>{
+    return(
+        <h1>RecipeDetailPage</h1>
+    )
+}
+export default RecipeDetailPage
---
+++ modulo3/cookenu/cookenu/src/pages/RecipesListPage/RecipesListPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const RecipesListPage =() =>{
+    return(
+        <h1>RecipesListPage</h1>
+    )
+}
+export default RecipesListPage
