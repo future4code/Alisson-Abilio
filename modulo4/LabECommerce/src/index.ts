4f89aaa92b48a45450e8047e12bad4c7ca60ea6e
alisson <alissonabilio@gmail.com>
2022-02-11 09:54:13 -0300

    realizando teste get e post

---
+++ modulo4/LabECommerce/src/index.ts
@@ -0,0 +1,42 @@
+import express, { Express, Request, Response } from "express";
+import { connection } from "./Data/connection";
+import cors from "cors";
+import { AddressInfo } from "net";
+
+
+
+const app: Express = express();
+app.use(express.json());
+app.use(cors());
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+       const address = server.address() as AddressInfo;
+       console.log(`Server is running in http://localhost: ${address.port}`);
+    } else {
+       console.error(`Failure upon starting server.`);
+    }
+});
+
+
+
+
+app.get("/usersAll", async (req:Request, res:Response) =>{
+    try{
+        const result = await connection.raw
+        (`select * from labecommerce_users;`)
+        res.status(200).send({result: result[0]})
+    }catch(error:any ){
+        res.status(500).send(error.sqlMessage || error.message)
+    }
+})
+
+app.post("/users", async(req:Request, res:Response)=>{
+    try{
+        const result = await connection.raw
+        const {id, name, email, password, adm} = req.body
+        (`insert into labecommerce_users value (
+            
+        ); `)
+    }
+})
