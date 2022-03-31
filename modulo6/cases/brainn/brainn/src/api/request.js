0de76b8a962b081ce892e8f7440b7505f7c11efa
alisson <alissonabilio@gmail.com>
2022-03-30 22:35:30 -0300

    map && filter

--- modulo6/cases/brainn/brainn/src/api/request.js
+++ modulo6/cases/brainn/brainn/src/api/request.js
@@ -1,9 +1,31 @@
 import axios from "axios";
-import { URL_BASE_LOTERIAS } from "../constants/url";
+import { URL_BASE_CONCURSOS, URL_BASE_CONCURSOSID, URL_BASE_LOTERIAS } from "../constants/url";
 
 export const getLoteria = (setData) => {
 
     axios.get(URL_BASE_LOTERIAS)
+
+        .then((res) => {
+            setData(res.data)
+        })
+        .catch((err) => {
+            alert(err.message)
+        })
+}
+export const getLoteriaConcursos = (setData) => {
+
+    axios.get(URL_BASE_CONCURSOS)
+
+        .then((res) => {
+            setData(res.data)
+        })
+        .catch((err) => {
+            alert(err.message)
+        })
+}
+export const getLoteriaConcursosId = (setData) => {
+
+    axios.get(URL_BASE_CONCURSOSID)
 
         .then((res) => {
             setData(res.data)
--- modulo6/cases/brainn/brainn/src/pages/Home/Home.js
+++ modulo6/cases/brainn/brainn/src/pages/Home/Home.js
@@ -1,38 +1,69 @@
 import React, { useEffect, useState } from "react";
-import { getLoteria } from "../../api/request";
+import { getLoteria, getLoteriaConcursos, getLoteriaConcursosId } from "../../api/request";
 import { BoxRight, BoxLeft, ContainerInferior, CotainnerGenaral, NameCenter, ContainnerSelector } from "./styled";
 
-export function getId(item) { return console.log( item.id ) }
+
+
 
 export const Home = () => {
     const [loteria, setLoteria] = useState([])
+    const [loteriaConcuso, setLoteriaConcuso] = useState([])
+    const [loteriaConcursoId, setLoteriaConcusoId] = useState([])
+
+    const [jogoAtual, set_JogoAtual] = useState({ id: 0, nome: "MEGA-SENA" })
 
     useEffect(() => {
         getLoteria(setLoteria)
+        getLoteriaConcursos(setLoteriaConcuso)
+        getLoteriaConcursosId(setLoteriaConcusoId)
     }, [])
 
+    const onChangeSelect = (e) => {
+        loteria.filter((item) => {
+            if (e.target.value === item.id.toString()) {
+                set_JogoAtual({ id: item.id, nome: item.nome.toUpperCase() })
+            }
+        })
+    }
+
     const result = loteria && loteria.map((item) => {
-        return <optionon Click={()=>{getId(item)}}>{item.nome}</optionon>
+        return <option key={item.id} value={item.id} >{
+            item.nome.toUpperCase()}</option>
+    })
+
+    const concurso = loteriaConcuso && loteriaConcuso.map((item) => {
+        if (item.loteriaId === jogoAtual.id) {
+            return item.loteriaId
+
+        }
+        
+    })
+    console.log("aqui id",concurso)
+    console.log("aqui tem id",loteriaConcursoId)
+
+    const concursoId = loteriaConcursoId && loteriaConcursoId.map((item) => {
+        if (item.loteria === concurso) {
+            return item.data
+        }
     })
     return (
         <CotainnerGenaral>
             <ContainnerSelector>
-                <select >
+                <select onChange={onChangeSelect}  >
                     {result}
                 </select>
             </ContainnerSelector>
             <ContainerInferior>
                 <BoxLeft>
                     <NameCenter>
-                        <p>{result[2]}</p>
+                        <p>{jogoAtual.nome}</p>
+                        <p>{concursoId}</p>
+                     
                     </NameCenter>
                 </BoxLeft>
                 <BoxRight>
-                    <p>1</p>
-                    <p>6</p>
-                    <p>4</p>
-                    <p>9</p>
-                    <p>0</p>
+                    <p>{concurso}</p>
+
                 </BoxRight>
             </ContainerInferior>
         </CotainnerGenaral>
